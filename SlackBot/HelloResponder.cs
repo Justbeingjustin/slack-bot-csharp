ba5721f431f296bff542943996c45e070b7fc908
Justin Gerber <Justbeingjustin@aol.com>
2019-06-09 12:46:23 -0700

    initial project push

---
+++ SlackBot/HelloResponder.cs
@@ -0,0 +1,23 @@
+﻿using MargieBot;
+using System.Text;
+
+namespace SlackBot
+{
+    public class HelloResponder : IResponder
+    {
+        public bool CanRespond(ResponseContext context)
+        {
+            return context.Message.MentionsBot
+                  && !context.BotHasResponded
+                  && context.Message.Text.ToLower().Contains("hello");
+        }
+
+        public BotMessage GetResponse(ResponseContext context)
+        {
+            var builder = new StringBuilder();
+            builder.Append("Hello ").Append(context.Message.User.FormattedUserID);
+
+            return new BotMessage { Text = builder.ToString() };
+        }
+    }
+}
---
+++ SlackBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using MargieBot;
+using System;
+using System.Configuration;
+
+namespace SlackBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Component.For<IResponder>().ImplementedBy<HelloResponder>());
+
+            var bot = new Bot();
+            var responders = container.ResolveAll<IResponder>();
+            foreach (var responder in responders)
+            {
+                bot.Responders.Add(responder);
+            }
+            var connect = bot.Connect(ConfigurationManager.AppSettings["SlackBotApiToken"]);
+
+            while (Console.ReadLine() != "close") { }
+        }
+    }
+}
